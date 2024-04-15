e40c957b0486559d3c905def01a4266f6d39316e
William F <110934475+unkokaeru@users.noreply.github.com>
2024-04-15 23:13:19 +0100

    Fixed import

--- src/content/module_page.py
+++ src/content/module_page.py
@@ -6,7 +6,8 @@ import re
 from logging import Logger
 from typing import cast
 
-from config.paths import HANDBOOK_PATH, OUTPUT_PATH, UNI_DOMAIN
+from config.constants import UNI_DOMAIN
+from config.paths import HANDBOOK_PATH, OUTPUT_PATH
 from src.processing.gpt_interaction import prompt_gpt
 from src.utilities.conversion_utilities import name_to_email, pdf_to_text
 from src.utilities.file_utilities import generate_markdown
